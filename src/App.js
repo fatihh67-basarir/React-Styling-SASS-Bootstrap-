f7c43ddebecde196ed414ec31eae392188ee0aa0
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-02 13:48:38 +0300

    button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
+import Button from './components/button/Button';
+import Card from './components/card/Card';
 
 function App() {
   return (
     <div className="App">
+      <Card/>
       
     </div>
   );
